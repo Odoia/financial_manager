8afc6647551d694a8731376273c50e9cf3c8b015
Tiago Henrique <tiagohenriqueodoia@gmail.com>
2020-03-08 20:06:38 -0300

    development Add a post to user
    -
    
    Signed-off-by: Tiago Henrique <tiagohenriqueodoia@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 const app = require('express')()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.status(200).send()
+})
+
+app.get('/users', (req, res) => {
+  const users = [
+    {name: 'john doe', email: 'jhon@mail.com'}
+  ]
+  res.status(200).json(users)
+})
+
+app.post('/users', (req, res) => {
+  res.status(201).json(req.body)
 })
 
 module.exports = app
