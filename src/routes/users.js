d57eb628016767fb9d6a1fd140f2ec2b950c5132
Tiago Henrique <tiagohenriqueodoia@gmail.com>
2020-04-05 19:09:38 -0300

    development Add a middlewares in users
    -
    
    Signed-off-by: Tiago Henrique <tiagohenriqueodoia@gmail.com>

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,14 +2,15 @@ module.exports = (app) => {
   const findAll = (req, res) => {
     app.services.user.findAll()
       .then(result => res.status(200).json(result))
+      .catch(err => next(err))
   }
 
-  const create = async (req, res) => {
+  const create = async (req, res, next) => {
     try {
       const result = await app.services.user.save(req.body)
       return res.status(201).json(result[0])
     } catch (err) {
-      return res.status(400).json({ error: err.message })
+      return next(err)
     }
   }
 
