5bda1972739548e16f0746053c81941f979fcca3
Tiago Henrique <tiagohenriqueodoia@gmail.com>
2020-04-08 08:11:31 -0300

    development Add a send token on tests
    -
    
    Signed-off-by: Tiago Henrique <tiagohenriqueodoia@gmail.com>

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -1,12 +1,23 @@
 const request = require('supertest')
-
+const jwt = require('jwt-simple')
 const app = require('../../src/app')
 
 const email = `${Date.now()}@mail.com`
+let user
 
+beforeAll(async () => {
+  const res = await app.services.user.save({
+    name: 'user account',
+    email: `${Date.now()}@mail.com`,
+    passwd: '123' 
+  })
+  user = { ...res[0] }
+  user.token = jwt.encode(user, 'secret')
+})
 
 test('must list all users', () => {
   return request(app).get('/users')
+    .set('authorization', `bearer ${user.token}`)
     .then((res) => {
       expect(res.status).toBe(200)
       expect(res.body.length).toBeGreaterThan(0)
@@ -16,6 +27,7 @@ test('must list all users', () => {
 test('must insert user successfully', () => {
   return request(app).post('/users')
     .send({ name: 'walter', email, passwd: '1234' })
+    .set('authorization', `bearer ${user.token}`)
     .then((res) => {
       expect(res.status).toBe(201)
       expect(res.body.name).toBe('walter')
@@ -26,6 +38,7 @@ test('must insert user successfully', () => {
 test('should insert a encrypted password', async () => {
   const res = await request(app).post('/users')
     .send({ name: 'new name', email: `${Date.now()}@mail.com`, passwd: '1234' })
+    .set('authorization', `bearer ${user.token}`)
   expect(res.status).toBe(201)
 
   const { id } = res.body
@@ -37,6 +50,7 @@ test('should insert a encrypted password', async () => {
 test('should not insert a user without name', () => {
   return request(app).post('/users')
     .send({ email: 'mail@mail.com', passwd: '1234' })
+    .set('authorization', `bearer ${user.token}`)
     .then((res) => {
       expect(res.status).toBe(400)
       expect(res.body.error).toBe('Name is require')
@@ -46,6 +60,7 @@ test('should not insert a user without name', () => {
 test('should not insert a user without email', async () => {
   const result = await request(app).post('/users')
     .send({ name: 'walter', passwd: '1234' })
+    .set('authorization', `bearer ${user.token}`)
   expect(result.status).toBe(400)
   expect(result.body.error).toBe('Email is require')
 })
@@ -53,6 +68,7 @@ test('should not insert a user without email', async () => {
 test('should not insert a user without password', (done) => {
   request(app).post('/users')
   .send({ name: 'walter', email: 'mail@mail.com' })
+    .set('authorization', `bearer ${user.token}`)
     .then((res) => {
       expect(res.status).toBe(400)
       expect(res.body.error).toBe('Password is require')
@@ -64,6 +80,7 @@ test('should not insert a user without password', (done) => {
 test('should not insert a user without name', () => {
   return request(app).post('/users')
     .send({ email: 'mail@mail.com', passwd: '1234' })
+    .set('authorization', `bearer ${user.token}`)
     .then((res) => {
       expect(res.status).toBe(400)
       expect(res.body.error).toBe('Name is require')
@@ -73,6 +90,7 @@ test('should not insert a user without name', () => {
 test('shoud not insert a duplicate email', () => {
   return request(app).post('/users')
     .send({ name: 'walter', email, passwd: '1234' })
+    .set('authorization', `bearer ${user.token}`)
     .then((res) => {
       expect(res.status).toBe(400)
       expect(res.body.error).toBe('Duplicate Email')
