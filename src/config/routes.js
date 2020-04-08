ee798a72155e8b679872e4d7dd388fedbe400423
Tiago Henrique <tiagohenriqueodoia@gmail.com>
2020-04-08 08:02:25 -0300

    development Add a signup rout and test
    -
    
    Signed-off-by: Tiago Henrique <tiagohenriqueodoia@gmail.com>

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,6 +2,9 @@ module.exports = (app) => {
   app.route('/auth/signin')
     .post(app.routes.auth.signin)
 
+  app.route('/auth/signup')
+    .post(app.routes.users.create)
+
   app.route('/users')
     .all(app.config.passport.authenticate())
     .get(app.routes.users.findAll)
--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -1,6 +1,17 @@
 const request = require('supertest')
 const app = require('../../src/app')
 
+test('should be creater user by signup', () => {
+  return request(app).post('/auth/signup')
+    .send({ name: 'jonh', email: `${Date.now()}@mail.com`, passwd: '123456' })
+    .then((res) => {
+      expect(res.status).toBe(201)
+      expect(res.body.name).toBe('jonh')
+      expect(res.body).toHaveProperty('email')
+      expect(res.body).not.toHaveProperty('passwd')
+    })
+})
+
 test('should be receiver a token at login', () => {
   const email = `${Date.now()}@mail.com`
   return app.services.user.save(
