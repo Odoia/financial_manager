c1e8a66f82cf4531609f44680456cab2b1b21db4
Tiago Henrique <tiagohenriqueodoia@gmail.com>
2020-05-22 11:18:48 -0300

    development Add new test
    -
    
    Signed-off-by: Tiago Henrique <tiagohenriqueodoia@gmail.com>

---
+++ src/migrations/20200504172803_create_table_transactions.js
@@ -0,0 +1,17 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('transactions', (t) => {
+    t.increments('id').primary()
+    t.string('description').notNull()
+    t.date('date').notNull()
+    t.decimal('ammount', 15, 2).notNull()
+    t.boolean('status').notNull().default(false)
+    t.integer('acc_id')
+      .references('id')
+      .inTable('accounts')
+      .notNull()
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('transactions')
+};
